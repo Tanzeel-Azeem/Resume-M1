2e97bbd3531e7e39263e8e7a377c9945b0e5b2d2
Tanzeel-Azeem <azeemtanzeel39@gmail.com>
2024-11-18 15:49:55 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+var generateResume = document.getElementById('generateResume');
+generateResume === null || generateResume === void 0 ? void 0 : generateResume.addEventListener('click', function () {
+    window.print();
+});
